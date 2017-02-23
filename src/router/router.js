08681e7a950251bb9ee4f04f68cff952cb24d290
yokiyokiyoki <419703726@qq.com>
2017-02-23 18:39:02 +0800

    增加了路由各宿舍名字指向和swiper，tab组件

--- src/router/router.js
+++ src/router/router.js
@@ -1,15 +1,45 @@
 import VueRouter from 'vue-router'
 import Vue from 'vue';
-const Page = {
-    template: '<div>page</div>'
+const glen = {
+    template: '<div style="font-size:50px;">Glen</div>'
+};
+const yujin = {
+    template: '<div style="font-size:50px;">浴巾</div>'
+};
+const zeyuan = {
+    template: '<div style="font-size:50px;">泽源</div>'
+};
+const timo = {
+    template: '<div style="font-size:50px;">timo</div>'
+};
+const zhuocheng = {
+    template: '<div style="font-size:50px;">卓承</div>'
+};
+const yoki = {
+    template: '<div style="font-size:50px;">yoki</div>'
 };
 // 导入路由
 Vue.use(VueRouter)
 // 定义路由
 const routes = [{
-    path: '/page',
-    component: Page
-}, ]
+    path: '/glen',
+    component: glen
+}, {
+    path: '/yujin',
+    component: yujin
+}, {
+    path: '/timo',
+    component: timo
+}, {
+    path: '/zeyuan',
+    component: zeyuan
+}, {
+    path: '/zhuocheng',
+    component: zhuocheng
+}, {
+    path: '/yoki',
+    component: yoki
+}]
 // 创建一个路由器实例
 const router = new VueRouter({
     routes
