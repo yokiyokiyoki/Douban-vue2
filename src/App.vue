d488a6e8343d5dc19cc4848c5176c5eabf97ef9a
yokiyokiyoki <419703726@qq.com>
2017-02-23 18:51:46 +0800

    删除了宿舍各人名字

--- src/App.vue
+++ src/App.vue
@@ -4,12 +4,7 @@
         <section-header></section-header>
         <p>hi{{msg}}</p>
         <!--路由链接-->
-        <router-link to="/glen">glen</router-link>
-        <router-link to="/timo">timo</router-link>
-        <router-link to="/yoki">yoki</router-link>
-        <router-link to="/yujin">浴巾</router-link>
-        <router-link to="/zeyuan">泽源</router-link>
-        <router-link to="/zhuocheng">卓承</router-link>
+
         <!--路由出口-->
         <router-view></router-view>
     </div>
